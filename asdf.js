f00ea9d9080ace9f94f739f9fe7aa6ea911fa1b3
Thijn Piera <pierathijn@gmail.com>
2018-12-19 14:48:03 +0100

    animatie van zoeken enzo

---
+++ asdf.js
@@ -0,0 +1,6 @@
+var zoekknop = document.querySelector('#zoeken');
+var header = document.querySelector('body > header');
+
+zoekknop.addEventListener('click', function () {
+    header.classList.toggle('show-form');
+});
